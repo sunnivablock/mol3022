df56142706879419015f6a60ed5ed4b5ae3f9c3a
Jenny Almestad <jennyalmestad@Jennys-MBP.local>
2021-02-19 13:04:30 +0100

    test

--- bioProsjekt/src/App.js
+++ bioProsjekt/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import page from "./components/page.jsx"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+         <page></page>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Bio prosjekt
         </p>
         <a
           className="App-link"
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bio Prosjekt
         </a>
       </header>
     </div>
---
+++ bioProsjekt/src/components/page.jsx
@@ -0,0 +1,10 @@
+import react from React
+
+const page = () => {
+
+    return(
+        <Text>Bio Prosjekt</Text>
+    )
+}
+
+export default page;
